5533eca23b4a7496f3bb8b6e832ff8b8f720fd3a
SpaceScape <71204304+Space-Scape@users.noreply.github.com>
2025-10-10 07:44:02 -0500

    Update eventwizard.py

--- eventwizard.py
+++ eventwizard.py
@@ -11,7 +11,6 @@ from discord import ButtonStyle
 from typing import Optional
 from datetime import datetime, timedelta, timezone, time
 from zoneinfo import ZoneInfo
-from gspread.exceptions import APIError, GSpreadException, CellNotFound
 
 # ---------------------------
 # 🔹 Google Sheets Setup
@@ -722,3 +721,4 @@ async def before_post_daily_schedule():
     await bot.wait_until_ready()
 
 bot.run(os.getenv('BOT_TOKEN'))
+
