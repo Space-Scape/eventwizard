833d7b009b12f2c348928eb3913fdb68c7a51079
SpaceScape <71204304+Space-Scape@users.noreply.github.com>
2025-11-12 08:50:45 -0600

    Update monopoly.py

--- monopoly.py
+++ monopoly.py
@@ -932,7 +932,7 @@ class MonopolyCog(commands.Cog):
         except Exception as e:
             print(f"Error setting teleblock status for {team_name}: {e}")
 
-    @app_commands.command(name="roll", description="Roll a dice (1-6)")
+    @app_commands.command(name="monopoly_roll", description="Roll a dice (1-6) for MONOPOLY")
     async def roll(self, interaction: discord.Interaction):
         if str(interaction.channel_id) not in TEAM_CHANNEL_IDS_AS_STR:
             await interaction.response.send_message(
