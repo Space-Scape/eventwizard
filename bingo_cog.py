d9c3fff3b6c7151f15c00cf5e16219108193b1b0
SpaceScape <71204304+Space-Scape@users.noreply.github.com>
2026-05-29 00:05:34 -0500

    Update bingo_cog.py

--- bingo_cog.py
+++ bingo_cog.py
@@ -58,9 +58,9 @@ BOSS_DROPS = {
     "Vet'ion": ["Vet'ion jr.", "Skull of vet'ion", "Dragon pickaxe", "Voidwaker blade", "Ring of the gods", "Skeleton champion scroll"],
     "Vorkath": ["Vorki", "Draconic visage", "Skeletal visage", "Jar of decay", "Dragonbone necklace"],
     "Yama": ["Yami", "Soulflame horn", "Oathplate helm", "Oathplate chest", "Oathplate legs", "Dossier"],
-    "Zulrah": ["Pet snakeling", "Tanzanite mutagen", "Magma mutagen", "Jar of swamp", "Tanzanite fang", "Magic fang", "Serpentine visage", "Uncut onyx"]
+    "Zulrah": ["Pet snakeling", "Tanzanite mutagen", "Magma mutagen", "Jar of swamp", "Tanzanite fang", "Magic fang", "Serpentine visage", "Uncut onyx"],
+    "Pets/Misc": ["Gull", "Kalphite princess", "Noon/Midnight", "Pet General Graardor", "Pet K'ril Tsutsaroth", "Pet Kraken", "Pet Kree'Arra", "Pet smoke devil", "Pet Zilyana", "Sraracha"]
 }
-
 
 class MessageImageSource(NamedTuple):
     attachment: Optional[discord.Attachment]
